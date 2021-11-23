0df4c52b2e35d61e532ae0e071b2eb18aca1dc9e
Dummyapt <duha.cinar@gmail.com>
2021-11-23 19:51:57 +0100

    Refactored code

--- src/main/java/de/dummyapt/sandbox/covidtable/CovidTable.java
+++ src/main/java/de/dummyapt/sandbox/covidtable/CovidTable.java
@@ -9,12 +9,15 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.sql.Date;
+import java.util.Arrays;
+
 public final class CovidTable extends Application {
     private final EntryDatabase repository = new EntryDatabase();
     private final TableView<Entry> table = new TableView<>();
-    private final TableColumn<Entry, String> name = new TableColumn<>("Entry");
-    private final TableColumn<Entry, String> value = new TableColumn<>("Value");
-    private final TableColumn<Entry, String> date = new TableColumn<>("Date");
+    private final TableColumn<Entry, String> name = new TableColumn<>("Name");
+    private final TableColumn<Entry, Double> value = new TableColumn<>("Value");
+    private final TableColumn<Entry, Date> date = new TableColumn<>("Date");
     private final Scene scene = new Scene(table);
     private final Image icon = new Image("https://cdn.pixabay.com/photo/2020/04/29/08/24/coronavirus-5107804_960_720.png");
 
@@ -24,9 +27,7 @@ public final class CovidTable extends Application {
         value.setCellValueFactory(new PropertyValueFactory<>("value"));
         date.setCellValueFactory(new PropertyValueFactory<>("date"));
 
-        table.getColumns().add(name);
-        table.getColumns().add(value);
-        table.getColumns().add(date);
+        table.getColumns().addAll(Arrays.asList(name, value, date));
         table.getItems().addAll(repository.getEntries());
         table.setPlaceholder(new Label("No data"));
 
--- src/main/java/de/dummyapt/sandbox/covidtable/EntryDatabase.java
+++ src/main/java/de/dummyapt/sandbox/covidtable/EntryDatabase.java
@@ -1,23 +1,42 @@
 package de.dummyapt.sandbox.covidtable;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 final class EntryDatabase {
+    private static final String URL = "jdbc:mariadb://[::1]:3306/corona?user=ebkherne";
+    private static final String QUERY = "SELECT lkname, inzidenz, datum FROM inzidenzen";
     private final ArrayList<Entry> entries = new ArrayList<>();
+    private Connection connection;
+    private Statement statement;
+    private ResultSet resultSet;
 
     EntryDatabase() {
         try {
-            var connection = DriverManager.getConnection("jdbc:mariadb://[::1]:3306/corona?user=ebkherne");
-            var statement = connection.createStatement();
-            var resultSet = statement.executeQuery("SELECT * FROM inzidenzen;");
+            connection = DriverManager.getConnection(URL);
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(QUERY);
             while (resultSet.next())
                 entries.add(new Entry(resultSet.getString("lkname"),
                         resultSet.getDouble("inzidenz"),
                         resultSet.getDate("datum")));
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            try {
+                assert resultSet != null;
+                assert statement != null;
+                assert connection != null;
+                resultSet.close();
+                statement.close();
+                connection.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
