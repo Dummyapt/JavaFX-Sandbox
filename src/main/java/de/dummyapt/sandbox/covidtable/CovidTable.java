daf13e7e3ec9462e61f78a7c1920c51d83e74127
Dummyapt <duha.cinar@gmail.com>
2021-11-30 11:25:11 +0100

    Refactored CovidTable.java and made database connection static

--- src/main/java/de/dummyapt/sandbox/covidtable/CovidTable.java
+++ src/main/java/de/dummyapt/sandbox/covidtable/CovidTable.java
@@ -1,7 +1,8 @@
 package de.dummyapt.sandbox.covidtable;
 
-import de.dummyapt.sandbox.covidtable.database.Database;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -13,55 +14,73 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.sql.Connection;
 import java.sql.Date;
 import java.sql.SQLException;
 import java.util.Arrays;
 
 public final class CovidTable extends Application {
-    private final BorderPane borderPane = new BorderPane();
-    private final Button refreshButton = new Button("_Refresh");
-    private final TableView<Entry> tableView = new TableView<>();
-    private final TableColumn<Entry, String> name = new TableColumn<>("Name");
-    private final TableColumn<Entry, Double> value = new TableColumn<>("Value");
-    private final TableColumn<Entry, Date> date = new TableColumn<>("Date");
-    private final Scene scene = new Scene(borderPane, 315, 400);
-    private final Image icon = new Image("https://cdn.pixabay.com/photo/2020/04/29/08/24/coronavirus-5107804_960_720.png");
-    private Database repository;
+    private Connection connection;
 
     public static void main(String[] args) {
         Application.launch(args);
     }
 
     @Override
-    public void init() throws SQLException {
-        repository = new Database("jdbc:mariadb://[::1]:3306/corona?user=ebkherne",
-                "SELECT * FROM inzidenzen;");
-        repository.connect();
+    public void init() {
+        try {
+            connection = Database.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void start(Stage stage) throws SQLException {
+        TableColumn<Entry, String> name = new TableColumn<>("Name");
+        name.setCellValueFactory(new PropertyValueFactory<>("name"));
+        TableColumn<Entry, Double> value = new TableColumn<>("Value");
+        value.setCellValueFactory(new PropertyValueFactory<>("value"));
+        TableColumn<Entry, Date> date = new TableColumn<>("Date");
+        date.setCellValueFactory(new PropertyValueFactory<>("date"));
+
+        TableView<Entry> tableView = new TableView<>();
+        tableView.getColumns().addAll(Arrays.asList(name, value, date));
+        tableView.setItems(getEntries());
+        tableView.setPlaceholder(new Label("No data"));
+
+        var refreshButton = new Button("_Refresh");
+        refreshButton.setOnAction(ae -> {
+            tableView.setItems(getEntries());
+            tableView.refresh();
+        });
+
+        var borderPane = new BorderPane();
         borderPane.setCenter(tableView);
         borderPane.setBottom(refreshButton);
         BorderPane.setAlignment(refreshButton, Pos.CENTER);
 
-        name.setCellValueFactory(new PropertyValueFactory<>("name"));
-        value.setCellValueFactory(new PropertyValueFactory<>("value"));
-        date.setCellValueFactory(new PropertyValueFactory<>("date"));
-
-        tableView.getColumns().addAll(Arrays.asList(name, value, date));
-        tableView.setItems(repository.getEntries());
-        tableView.setPlaceholder(new Label("No data"));
-
-        refreshButton.setOnAction(ae -> {
-            tableView.setItems(repository.getEntries());
-            tableView.refresh();
-        });
-
+        var scene = new Scene(borderPane, 315, 400);
+        var icon = new Image("https://cdn.pixabay.com/photo/2020/04/29/08/24/coronavirus-5107804_960_720.png");
         stage.setScene(scene);
         stage.setTitle("Covid Table");
         stage.setResizable(false);
         stage.getIcons().add(icon);
         stage.show();
+    }
+
+    private ObservableList<Entry> getEntries() {
+        ObservableList<Entry> entries = FXCollections.observableArrayList();
+        try {
+            var statement = connection.createStatement();
+            var resultSet = statement.executeQuery("SELECT * FROM inzidenzen");
+            while (resultSet.next())
+                entries.add(new Entry(resultSet.getString("lkname"),
+                        resultSet.getDouble("inzidenz"),
+                        resultSet.getDate("datum")));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return entries;
     }
 }
---
+++ src/main/java/de/dummyapt/sandbox/covidtable/Database.java
@@ -0,0 +1,20 @@
+package de.dummyapt.sandbox.covidtable;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class Database {
+    private static final String URL = "jdbc:mariadb://[::1]";
+    private static final String DB = "corona";
+    private static final String PORT = "3306";
+    private static final String USER = "ebkherne";
+    private static final String PASS = "";
+    private static Connection connection;
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null)
+            connection = DriverManager.getConnection(URL + ':' + PORT + '/' + DB, USER, PASS);
+        return connection;
+    }
+}
