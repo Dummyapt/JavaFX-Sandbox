830ca231ac1ee686748316e1b3e5af788111d796
Duha.Cinar <duha.cinar@gmail.com>
2021-12-14 10:51:22 +0100

    Added New page-functionality

--- src/main/java/de/dummyapt/sandbox/covidtable/CovidTable.java
+++ src/main/java/de/dummyapt/sandbox/covidtable/CovidTable.java
@@ -1,94 +1,28 @@
 package de.dummyapt.sandbox.covidtable;
 
-import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
 
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.SQLException;
 import java.util.Arrays;
 
-public final class CovidTable extends Application {
-    private final BorderPane borderPane = new BorderPane();
+public final class CovidTable {
+    private final TableView<Entry> tableView;
     private Connection connection;
 
-    public static void main(String[] args) {
-        Application.launch(args);
-    }
-
-    @Override
-    public void init() {
+    public CovidTable() {
         try {
             connection = Database.getConnection();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-    }
 
-    @Override
-    public void start(Stage stage) throws SQLException {
-        var showDataItem = new MenuItem("_Data");
-        showDataItem.setGraphic(new ImageView(new Image("file:src/main/resources/covidtable/images/show.png")));
-        showDataItem.setOnAction(ae -> borderPane.setCenter(getTableView()));
-
-        var newEntryItem = new MenuItem("_New");
-        newEntryItem.setGraphic(new ImageView(new Image("file:src/main/resources/covidtable/images/new.png")));
-
-        var exitItem = new MenuItem("_Exit");
-        exitItem.setGraphic(new ImageView(new Image("file:src/main/resources/covidtable/images/exit.png")));
-        exitItem.setOnAction(ae -> stage.close());
-
-        var lightModeItem = new CheckMenuItem("_Light mode");
-        lightModeItem.setSelected(true);
-
-        var darkModeItem = new MenuItem("Dark mode");
-        darkModeItem.setDisable(true);
-
-        var infoItem = new MenuItem("_Info");
-        infoItem.setGraphic(new ImageView(new Image("file:src/main/resources/covidtable/images/about.png")));
-        infoItem.setOnAction(ae -> borderPane.setCenter(new Label("Made by Dummyapt")));
-
-        var helpItem = new MenuItem("_Help");
-        helpItem.setGraphic(new ImageView(new Image("file:src/main/resources/covidtable/images/help.png")));
-        helpItem.setOnAction(ae -> getHostServices().showDocument("mailto://support@dummyapt.de"));
-
-        var fileMenu = new Menu("_File");
-        fileMenu.getItems().addAll(showDataItem, newEntryItem, exitItem);
-        var viewMenu = new Menu("_View");
-        viewMenu.getItems().addAll(lightModeItem, darkModeItem);
-        var aboutMenu = new Menu("_About");
-        aboutMenu.getItems().addAll(infoItem, helpItem);
-
-        var menuBar = new MenuBar();
-        menuBar.getMenus().addAll(fileMenu, viewMenu, aboutMenu);
-
-        var refreshButton = new Button("_Refresh");
-        refreshButton.setGraphic(new ImageView(new Image("file:src/main/resources/covidtable/images/refresh.png")));
-        refreshButton.setOnAction(ae -> borderPane.setCenter(getTableView()));
-
-        borderPane.setTop(menuBar);
-        borderPane.setCenter(getTableView().getPlaceholder());
-        borderPane.setBottom(refreshButton);
-        BorderPane.setAlignment(refreshButton, Pos.CENTER);
-
-        var scene = new Scene(borderPane, 360, 350);
-        stage.getIcons().add(new Image("https://cdn-icons-png.flaticon.com/512/2913/2913604.png"));
-        stage.setTitle("CovidTable");
-        stage.setResizable(false);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    private TableView<Entry> getTableView() {
         var lkId = new TableColumn<Entry, Integer>("lkID");
         lkId.setCellValueFactory(new PropertyValueFactory<>("lkId"));
         var lkName = new TableColumn<Entry, String>("LKName");
@@ -98,33 +32,35 @@ public final class CovidTable extends Application {
         var date = new TableColumn<Entry, Date>("Date");
         date.setCellValueFactory(new PropertyValueFactory<>("date"));
 
-        var tableView = new TableView<Entry>();
+        tableView = new TableView<>();
         tableView.getColumns().addAll(Arrays.asList(lkId, lkName, value, date));
         tableView.setPlaceholder(new Label("No data"));
-        tableView.setItems(getEntries());
-        tableView.refresh();
+    }
 
+    TableView<Entry> getTableView() {
+        tableView.setItems(getEntries());
         return tableView;
     }
 
     private ObservableList<Entry> getEntries() {
         ObservableList<Entry> entries = FXCollections.observableArrayList();
         try {
-            var statement = connection.createStatement();
-            var resultSet = statement.executeQuery("SELECT * FROM corona.inzidenzen");
+            var resultSet = connection.createStatement().executeQuery("SELECT * FROM corona.inzidenzen;");
             while (resultSet.next())
-                entries.add(
-                        new Entry(
-                                resultSet.getInt("id"),
-                                resultSet.getInt("lkid"),
-                                resultSet.getString("lkname"),
-                                resultSet.getDouble("inzidenz"),
-                                resultSet.getDate("datum")
-                        )
-                );
+                entries.add(new Entry(resultSet.getInt("id"), resultSet.getInt("lkid"), resultSet.getString("lkname"), resultSet.getDouble("inzidenz"), resultSet.getDate("datum")));
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return entries;
+    }
+
+    boolean addEntry(int lkId, String lkName, double value, Date date) {
+        try {
+            connection.createStatement().executeQuery("INSERT INTO corona.inzidenzen (lkid, lkname, inzidenz, datum) " + String.format("VALUES (%s, '%s', %s, '%s');", lkId, lkName, value, date));
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
